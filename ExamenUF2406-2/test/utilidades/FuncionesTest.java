27d68cf0b09f66a16779e71eef0c6e0f83898630
Araceli <Araceli@DESKTOP-BBV7U01>
2022-08-16 13:24:10 +0200

    Examen terminado uf2406-2

--- ExamenUF2406-2/test/utilidades/FuncionesTest.java
+++ ExamenUF2406-2/test/utilidades/FuncionesTest.java
@@ -15,16 +15,16 @@ class FuncionesTest {
 	 
 	@Test
 	void devuelveCentralTest() {
-		assertEquals(5, f.devuelveCentral(5,1,9), "El número central de los números 5,1 y 9 es el número 5");
-		assertEquals(1, f.devuelveCentral(-5,1,9), "El número central de los números -5,1 y 9 es el número 1");
-		assertEquals(-1, f.devuelveCentral(5,-9,-1), "El número central de los números 5,-9 y -1 es el número -1");
+		assertEquals(5, f.devuelveCentral(5,1,9), "El nÃºmero central de los nÃºmeros 5,1 y 9 es el nÃºmero 5");
+		assertEquals(1, f.devuelveCentral(-5,1,9), "El nÃºmero central de los nÃºmeros -5,1 y 9 es el nÃºmero 1");
+		assertEquals(-1, f.devuelveCentral(5,-9,-1), "El nÃºmero central de los nÃºmeros 5,-9 y -1 es el nÃºmero -1");
 	}
 	@Test
 	void esBisiestoTest() {
-		assertTrue(f.esBisiesto(2020),"El número debe devolver true");
-		assertFalse(f.esBisiesto(1990), "El número debe devolver false");
-		assertFalse(f.esBisiesto(2100), "El número debe devolver false");
-		assertTrue(f.esBisiesto(1660),"Debe saltar una excepción contempleada");
-		assertFalse(f.esBisiesto(2502),"Debe saltar una excepción contempleada");
+		assertTrue(f.esBisiesto(2020),"El nÃºmero debe devolver true");
+		assertFalse(f.esBisiesto(1990), "El nÃºmero debe devolver false");
+		assertFalse(f.esBisiesto(2100), "El nÃºmero debe devolver false");
+		assertTrue(f.esBisiesto(1660),"Debe saltar una excepciÃ³n contempleada");
+		assertFalse(f.esBisiesto(2502),"Debe saltar una excepciÃ³n contempleada");
 	}
 }
